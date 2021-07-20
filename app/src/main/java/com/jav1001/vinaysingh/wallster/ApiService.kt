3a09062ade953146c3e987528041c87b1e250088
Vinay Singh <vinaysingh8866@gmail.com>
2021-07-20 13:33:15 -0400

    Main Activity Category Buttons added for colors and nature.

--- app/src/main/java/com/jav1001/vinaysingh/wallster/ApiService.kt
+++ app/src/main/java/com/jav1001/vinaysingh/wallster/ApiService.kt
@@ -1,4 +1,12 @@
 package com.jav1001.vinaysingh.wallster
+import retrofit2.http.GET
+import retrofit2.http.Path
+interface ApiService {
 
-class ApiService {
+    @GET("search?q=vape")
+    suspend fun getWallpaper(): PokemonListWrapper
+
+//https://wallhaven.cc/api/v1/search?q=vape
+
+
 }
--- app/src/main/java/com/jav1001/vinaysingh/wallster/data/WallPaperData.kt
+++ app/src/main/java/com/jav1001/vinaysingh/wallster/data/WallPaperData.kt
@@ -1,5 +1,38 @@
 package com.jav1001.vinaysingh.wallster.data
 
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+
 class WallPaperData {
-    
+    @JsonClass(generateAdapter = true)
+
+    data class WallpaperSearch(
+        @Json(name = "data")
+        val results://
+    )
+
+
+    data class WallPaperInfo(
+        val id: Int,
+        val url: String,
+        val short_url: Int,
+        views Int
+        favorites Int,
+        source,
+        purity,
+        category,
+        dimension_x Int,
+        dimension_y Int,
+        resolution,
+        ratio,
+        file_size Int,
+        file_type,
+        created_at,
+        val colors : List<String>,
+        path:,
+
+    )
+
+
+
 }
