c9ef5eac5e7818fa1f936994fac64b691e63b2b3
Vinay Singh <vinaysingh8866@gmail.com>
2021-08-09 15:46:43 -0400

    Service Locator, Repository and data class added

--- app/src/main/java/com/jav1001/vinaysingh/wallster/ApiService.kt
+++ app/src/main/java/com/jav1001/vinaysingh/wallster/ApiService.kt
@@ -2,8 +2,11 @@ package com.jav1001.vinaysingh.wallster
 import com.jav1001.vinaysingh.wallster.data.WallpaperList
 import retrofit2.http.GET
 import retrofit2.http.Path
+import retrofit2.http.Query
+
 interface ApiService {
-    @GET("search?q=vape")
-    suspend fun getWallpaper():WallpaperList
+    @GET("search?q=dogs")
+    suspend fun getWallpaper(@Query("q") queryKey: String = "",
+                             @Query("categories") categories: String = "111"):WallpaperList
 //https://wallhaven.cc/api/v1/search?q=vape
 }
